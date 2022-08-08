22e9f7e1f6788e03a6ff97a47368ef02e56a8e52
Савва Чальцев <cavvach@gmail.com>
2022-08-08 12:45:59 +0300

    Инициализация переменной!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,23 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace ConsoleApp1
+class MainClass
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string name = "savva";
-            name.Increment();
-            Console.WriteLine(name);
-        }
-        
-    }
-    public static class MyHelper
-    {
-        public static void Increment(this string s)
-        {
-            s = "irma";
-        }
-    }
+	public static void Main(string[] args)
+	{
+
+		string myName;
+		myName = "Savva";
+
+		Console.WriteLine(myName);
+
+		Console.ReadKey();
+	}
 }
