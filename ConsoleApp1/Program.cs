6c6e29718fdf47bb83e59fc74e8995e38945a083
Савва Чальцев <cavvach@gmail.com>
2022-08-08 12:44:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "savva";
+            name.Increment();
+            Console.WriteLine(name);
+        }
+        
+    }
+    public static class MyHelper
+    {
+        public static void Increment(this string s)
+        {
+            s = "irma";
+        }
+    }
+}
